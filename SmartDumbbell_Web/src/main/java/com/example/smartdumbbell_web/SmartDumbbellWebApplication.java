f84eeff79538560c90c27ae5eec8be96c7ffde8f
ggyu0629 <101187523+ggyu0629@users.noreply.github.com>
2023-06-21 14:00:31 +0900

    server upload

---
+++ SmartDumbbell_Web/src/main/java/com/example/smartdumbbell_web/SmartDumbbellWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartdumbbell_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDumbbellWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartDumbbellWebApplication.class, args);
+    }
+
+}
