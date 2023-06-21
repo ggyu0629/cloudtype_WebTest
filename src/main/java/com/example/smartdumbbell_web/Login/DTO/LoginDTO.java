f9541ba657e372225ed841cb77572313b5e21922
ggyu0629 <101187523+ggyu0629@users.noreply.github.com>
2023-06-21 14:25:44 +0900

    Add files via upload

---
+++ src/main/java/com/example/smartdumbbell_web/Login/DTO/LoginDTO.java
@@ -0,0 +1,10 @@
+package com.example.smartdumbbell_web.Login.DTO;
+
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+    String id;
+    String password;
+}
---
+++ src/main/java/com/example/smartdumbbell_web/Login/DTO/SignUpDTO.java
@@ -0,0 +1,12 @@
+package com.example.smartdumbbell_web.Login.DTO;
+
+import lombok.Data;
+
+@Data
+public class SignUpDTO {
+    String id;
+    String password;
+    String name;
+    String role;
+    String institution;
+}
